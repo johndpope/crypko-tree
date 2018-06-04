08ab19da0a0c68f4dc8c6248a2ccb4a977b771f3
Shutaro Takimoto <shu@tak.im>
2018-06-05 00:49:14 +0900

    feat: show crypko image

--- pages/crypko.js
+++ pages/crypko.js
@@ -1,18 +1,46 @@
 import { PureComponent } from 'react';
 import fetch from 'isomorphic-unfetch';
+import sha1 from 'sha1';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 
 const URI_CRIPKO_API = 'https://api.crypko.ai';
+const URI_CRIPKO_STATIC = 'https://img.crypko.ai/daisy/';
+const URI_CRIPKO_GOOGLE = 'https://storage.googleapis.com/img.crypko.ai/daisy/';
+const HASH_SECRET = 'asdasd3edwasd';
+
+const detailMemo = {};
 async function fetchDetail(crypkoId) {
-  return fetch(`${URI_CRIPKO_API}/crypkos/${crypkoId}/detail`);
+  if (!detailMemo[crypkoId]) {
+    const res = await fetch(`${URI_CRIPKO_API}/crypkos/${crypkoId}/detail`);
+    const json = await res.json();
+    detailMemo[crypkoId] = json;
+  }
+  return detailMemo[crypkoId];
+}
+
+function getImageUri(crypko, size) {
+  let base;
+  switch (size) {
+    case 'sm':
+    case 'lg':
+      base = URI_CRIPKO_STATIC;
+      break;
+    case 'xsm':
+      base = URI_CRIPKO_GOOGLE;
+      break;
+    default:
+      throw new Error(`getImageUri: unknown size "${size}"`);
+  }
+
+  const hash = sha1(`${crypko.noise}${HASH_SECRET}${crypko.attrs}`);
+  return `${base}${hash}_${size}.jpg`;
 }
 
 class Crypko extends PureComponent {
   static async getInitialProps({ query }) {
     const { id } = query;
-    const res = await fetchDetail(id);
-    const crypkoProps = await res.json();
+    const crypkoProps = await fetchDetail(id);
     return crypkoProps;
   }
 
@@ -28,16 +56,12 @@ class Crypko extends PureComponent {
           as={`/c/${matron.id}`}
           prefetch
         >
-          {matron.name || `(${matron.id})`}
+          <a href=".">{matron.name || `(${matron.id})`}</a>
         </Link>
       );
       sireLink = (
-        <Link
-          href={{ pathname: '/crypko', query: { id: sire.id } }}
-          as={`/c/${sire.id}`}
-          prefetch
-        >
-          {sire.name || `(${sire.id})`}
+        <Link href={{ pathname: '/crypko', query: { id: sire.id } }} as={`/c/${sire.id}`} prefetch>
+          <a href=".">{sire.name || `(${sire.id})`}</a>
         </Link>
       );
     } else {
@@ -50,6 +74,7 @@ class Crypko extends PureComponent {
         <p>Crypko: {name || `(${id})`}</p>
         <p>Matron: {matronLink}</p>
         <p>Sire: {sireLink}</p>
+        <img src={getImageUri(this.props, 'lg')} alt="" />
       </div>
     );
   }
